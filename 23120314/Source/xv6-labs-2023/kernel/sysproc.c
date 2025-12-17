ded18cc1ae5d8807d63a8f889b43dc82d845e353
pqkzz <23120283@student.hcmus.edu.vn>
2025-12-17 12:55:04 +0700

    add

---
+++ 23120314/Source/xv6-labs-2023/kernel/sysproc.c
@@ -0,0 +1,134 @@
+#include "types.h"
+#include "riscv.h"
+#include "param.h"
+#include "defs.h"
+#include "memlayout.h"
+#include "spinlock.h"
+#include "proc.h"
+
+uint64
+sys_exit(void)
+{
+  int n;
+  argint(0, &n);
+  exit(n);
+  return 0;  // not reached
+}
+
+uint64
+sys_getpid(void)
+{
+  return myproc()->pid;
+}
+
+uint64
+sys_fork(void)
+{
+  return fork();
+}
+
+uint64
+sys_wait(void)
+{
+  uint64 p;
+  argaddr(0, &p);
+  return wait(p);
+}
+
+uint64
+sys_sbrk(void)
+{
+  uint64 addr;
+  int n;
+
+  argint(0, &n);
+  addr = myproc()->sz;
+  if(growproc(n) < 0)
+    return -1;
+  return addr;
+}
+
+uint64
+sys_sleep(void)
+{
+  int n;
+  uint ticks0;
+
+
+  argint(0, &n);
+  acquire(&tickslock);
+  ticks0 = ticks;
+  while(ticks - ticks0 < n){
+    if(killed(myproc())){
+      release(&tickslock);
+      return -1;
+    }
+    sleep(&ticks, &tickslock);
+  }
+  release(&tickslock);
+  return 0;
+}
+
+
+#ifdef LAB_PGTBL
+// here
+// new
+uint64
+sys_pgaccess(void)
+{
+  uint64 start_addr;
+  int count;
+  uint64 mask;
+
+  argaddr(0, &start_addr);
+  argint(1, &count);
+  argaddr(2, &mask);
+
+  if (count > 64 || count < 0) {
+    return -1;
+  }
+
+  struct proc *p = myproc();
+  uint64 kmask = 0;
+
+  for (int i = 0; i < count; i++) {
+    uint64 current_va = start_addr + i * PGSIZE;
+
+    pte_t *pte = walk(p->pagetable, current_va, 0);
+    
+    // PTE_V: valid bit
+    if (pte && (*pte & PTE_V) && (*pte & PTE_A)) {
+      kmask |= (1L << i);
+      *pte &= ~PTE_A;
+    }
+  }
+
+  if (copyout(p->pagetable, mask, (char *)&kmask, sizeof(kmask)) < 0) {
+    return -1;
+  }
+
+  return 0;
+}
+#endif
+
+uint64
+sys_kill(void)
+{
+  int pid;
+
+  argint(0, &pid);
+  return kill(pid);
+}
+
+// return how many clock tick interrupts have occurred
+// since start.
+uint64
+sys_uptime(void)
+{
+  uint xticks;
+
+  acquire(&tickslock);
+  xticks = ticks;
+  release(&tickslock);
+  return xticks;
+}
